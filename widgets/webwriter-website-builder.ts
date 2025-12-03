263dbdcef74ca4e7f2bdfc8526a51a7761213f9d
moritz.langenhan <moritz.langenhan@rwth-aachen.de>
2025-12-03 13:13:30 +0100

    barebone construct

---
+++ widgets/webwriter-website-builder.ts
@@ -0,0 +1,33 @@
+import {html, css} from "lit"
+import {LitElementWw} from "@webwriter/lit"
+import {customElement} from "lit/decorators.js"
+
+
+/* Optional LOCALIZATION: Uncomment this after first running `npm run localize` in the command line.
+import LOCALIZE from '../localization/generated'
+import {msg} from '@lit/localize'
+*/
+
+@customElement("webwriter-website-builder")
+export class WebwriterWebsiteBuilder extends LitElementWw {
+
+  /* Optional LOCALIZATION: Uncomment this after first running `npm run localize` in the command line.
+  localize = LOCALIZE
+  */
+
+  /** Register the classes of custom elements to use in the Shadow DOM here.
+   * @example
+   * import SlButton from "@shoelace-style/shoelace/dist/components/button/button.component.js"
+   * ...
+   *   static scopedElements = {"sl-button": SlButton}
+   **/
+  static scopedElements = {}
+
+  /** Put the styles for your Shadow DOM (what is rendered through render()) here. */
+  static styles = css``
+
+  /** Define your template here and return it. */
+  render() {
+    return html`Hello, world!!!`
+  }
+}
